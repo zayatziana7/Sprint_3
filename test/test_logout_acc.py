ad8804ae6f1363fce642e14c258dea44e9662d8d
Elieeeya <ik.slydnev@gmail.com>
2023-02-04 22:25:28 +0300

    final project test

---
+++ test/test_logout_acc.py
@@ -0,0 +1,21 @@
+from conftest import *
+from utils import *
+
+
+class TestLogaot:
+    def test_click_link_constructor_true(self, driver):
+        driver.get(base_url)
+        driver.find_element(*AppHeader.link_acc).click()
+        wait_el(driver, AuthorizationForm.title_login_page)
+        driver.find_element(*AuthorizationForm.email).send_keys(email)
+        driver.find_element(*AuthorizationForm.password).send_keys(password)
+        driver.find_element(*AuthorizationForm.button_login).click()
+        wait_el(driver, MainPage.title_main_page)
+        driver.find_element(*AppHeader.link_acc).click()
+        wait_el(driver, ProfilePage.button_exit)
+        driver.find_element(*ProfilePage.button_exit).click()
+        wait_el(driver, AuthorizationForm.title_login_page)
+        current_url = driver.current_url
+
+        assert current_url == base_url + 'login'
+
---
+++ test/test_navigate_personal_acc.py
@@ -0,0 +1,17 @@
+from conftest import *
+from utils import *
+
+
+class TestNavigateToPersonalAccount:
+    def test_navigate_to_pesronal_account_true(self, driver):
+        driver.get(base_url)
+        driver.find_element(*AppHeader.link_acc).click()
+        wait_el(driver, AuthorizationForm.title_login_page)
+        driver.find_element(*AuthorizationForm.email).send_keys(email)
+        driver.find_element(*AuthorizationForm.password).send_keys(password)
+        driver.find_element(*AuthorizationForm.button_login).click()
+        wait_el(driver, MainPage.title_main_page)
+        driver.find_element(*AppHeader.link_acc).click()
+        current_url = driver.current_url
+        assert current_url == base_url + 'account'
+
